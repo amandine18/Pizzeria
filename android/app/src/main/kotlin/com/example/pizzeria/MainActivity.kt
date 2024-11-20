1c02bb72718f67cc9b1a46f300193116be62a20a
Amandine DURAND <durandamandine@icloud.com>
2024-11-20 12:15:56 +0100

    update all main branch

--- android/app/src/main/kotlin/com/example/pizzeria/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pizzeria
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/models/option_item.dart
+++
@@ -1,7 +0,0 @@
-class OptionItem {
-  final int value;
-  final String name;
-  final int supplement;
-
-  OptionItem(this.value, this.name, {this.supplement = 0});
-}
